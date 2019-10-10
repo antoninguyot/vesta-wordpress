79331aca046890987dfb5b506ee33e15bf417883
Antonin <antonin.guyot@icloud.com>
2019-10-10 19:24:41 +0200

    better organization

--- hosting_wp.sh
+++ hosting_wp.sh
@@ -1,36 +1,47 @@
 #!/bin/bash
 
+######################
+#  Config variables  #
+######################
 USER="$1"
 DB_NAME="wp"
 WP_FILES="/tmp/wp"
 WP_PATH="$5"
 
+######################
+#      Database      #
+######################
 DB_PASSWORD="$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13)"
+/usr/local/vesta/bin/v-add-database "$USER" "$DB_NAME" "wp" "$DB_PASSWORD"
 
+######################
+#  Downloading files #
+######################
 mkdir -p "$WP_FILES"
 
 if [ ! -f "$WP_FILES"/wordpress.zip ]; then
 	wget -O "$WP_FILES"/wordpress.zip https://wordpress.org/latest.zip
 fi
-
 if [ ! -f "$WP_FILES"/woocommerce.zip ]; then
 	wget -O "$WP_FILES"/woocommerce.zip https://github.com/woocommerce/woocommerce/archive/3.7.1.zip
 fi
 
+######################
+# Wordpress install  #
+######################
+
 cp "$WP_FILES"/wordpress.zip "$WP_PATH/" || exit 1
-
 cd "$WP_PATH" || exit 1
-
 rm -f index.html
-
 unzip -q wordpress.zip
-
 mv wordpress/* .
 rmdir --ignore-fail-on-non-empty wordpress
 
-# DB creation
-/usr/local/vesta/bin/v-add-database "$USER" "$DB_NAME" "wp" "$DB_PASSWORD"
+######################
+#  Wordpress config  #
+######################
 
+# Database configuration
 echo "<?php
 define('DB_NAME', '${USER}_${DB_NAME}');
 define('DB_USER', '${USER}_${DB_NAME}');
@@ -47,13 +58,20 @@ if ( !defined('ABSPATH') )
 require_once(ABSPATH . 'wp-settings.php');
 require_once(ABSPATH . 'ateros-config.php');" > wp-config.php
 
+# Downloading the custom Ateros Textile config file
 curl https://raw.githubusercontent.com/anto2oo/ateros-wordpress/master/ateros-config.php > ateros-config.php
 
+######################
+#  Plugins install   #
+######################
+
 cd wp-content/plugins || exit
-
 cp -f "$WP_FILES"/woocommerce.zip . || exit 1
-
 unzip -q woocommerce.zip
 rm -f woocommerce.zip
+
+######################
+#      Others        #
+######################
 
 chown -R "$USER":"$USER" ./*
